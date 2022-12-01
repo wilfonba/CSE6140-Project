cb9b4463dc75ea48e477f61309e60b38435faad4
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-12-01 09:31:16 -0500

    Small improvements to BnB
    Adding approx to LB. Trying to further constrain BnB to improve performance.

--- BnB.py
+++ BnB.py
@@ -30,6 +30,8 @@ def BnB(inst, alg, cutOff, rSeed, G):
             break
 
     # Begin BnB
+    # Preprocess G to remove isolated nodes
+    G.remove_nodes_from(list(nx.isolates(G)))
     # Initialize variables
     C_init = []              # list of the vertices for the Vertex Cover to be returned
     C_list = [[], []]
@@ -39,6 +41,7 @@ def BnB(inst, alg, cutOff, rSeed, G):
     initial_level = 0   # Starting index for subproblem
     counter = itertools.count()           # Count when entering heap
     count = next(counter)
+    min_bound = compute_lower_bound_simple(G)  # Minimum feasible bound
 
     # Push starting problem to heap
     pq = []
@@ -78,10 +81,13 @@ def BnB(inst, alg, cutOff, rSeed, G):
                     printTraceFile(len(C), duration, traceFile)
             else:  # Add new subproblem to heap
                 if level + 1 <= len(list(G.nodes)):  # Ensure level is inbounds
-                    G_prime = G.subgraph(G.nodes - C)
-                    new_lower_bound = len(C) + compute_lower_bound(G_prime)
+                    G_prime = G.subgraph(list(G.nodes)[level:])
+                    new_lower_bound = len(
+                        C) + compute_lower_bound(cutOff, rSeed, G_prime)
+                    # new_lower_bound = len(C) + compute_lower_bound_simple(G_prime)
                     if new_lower_bound < sizeB:
-                        hq.heappush(pq, (new_lower_bound, count, C, level))
+                        if new_lower_bound >= min_bound:
+                            hq.heappush(pq, (new_lower_bound, count, C, level))
 
     duration = time.time() - start
 
@@ -97,7 +103,7 @@ def BnB(inst, alg, cutOff, rSeed, G):
 ###############################################################
 
 
-def compute_lower_bound(G):
+def compute_lower_bound_simple(G):
     # Simple lower bound given by K&T 10.2, pg. 556
     nodes = len(list(G.nodes))
     edges = len(list(G.edges))
@@ -106,3 +112,63 @@ def compute_lower_bound(G):
         k = nodes
 
     return k
+
+###############################################################
+
+
+def compute_lower_bound(cutOff, rSeed, G):
+    # Adapted from Seongmin's approximation algorithm
+    random.seed(rSeed)
+
+    i = 0  # standard iterator
+
+    G1 = G.copy()
+    C = []  # list of the vertices for the Vertex Cover to be returned
+    # getEdge = getRandomEdge
+    getEdge = getMaxDegreeEdge
+
+    start = time.time()
+    while time.time() - start < cutOff:
+        # select an edge (u,v) to be added to C
+        E = G1.edges
+        if len(E) == 0:  # C covers all the edges in G1
+            break
+
+        # Greedily get the edge to be added to C
+        (u, v) = getEdge(E, G1)
+
+        # remove the two endpoints u and v from the graph G1
+        G1.remove_node(u)
+        G1.remove_node(v)
+        assert u not in C
+        assert v not in C
+        C.append(u)
+        C.append(v)
+
+    duration = time.time() - start
+
+    # Trace File for the Greedy Approx algorithm is just one line (https://piazza.com/class/l725zf0sivy53i/post/151_f12)
+    # assert checker(G, C)
+    sizeC = len(C)
+    lowerboundC = int(math.ceil(1/2 * sizeC))
+
+    return lowerboundC
+
+
+def getRandomEdge(E, G1) -> tuple:
+    return random.choice(list(E))
+
+
+def getMaxDegreeEdge(E, G1) -> tuple:
+    maxDegree = 0
+    max_u, max_v = -1, -1
+    for e in E:
+        u, v = e[0], e[1]
+        u_degree = G1.degree[u]
+        v_degree = G1.degree[v]
+        degree = u_degree + v_degree
+        if maxDegree < degree:
+            maxDegree = degree
+            max_u = u
+            max_v = v
+    return max_u, max_v
