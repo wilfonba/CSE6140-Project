53bacf953a83570a058c448a860d84a57bba2757
Anand <anand@lawn-128-61-50-41.lawn.gatech.edu>
2022-12-02 00:48:19 -0500

    Part 1 + Part 2 Plots Done

--- binData.py
+++ binData.py
@@ -10,42 +10,109 @@ ALG = ['LS1','LS2']
 exact = 4542
 nBins = 11*cutOff
 
-for alg in ALG:
-    bins = np.zeros((nBins,100))
-    count = np.zeros((nBins,100))
 
+for alg in ALG:
     f = open(alg + "_bins.txt",'w')
     traces = []
     data = []
     path = 'OutputFiles/' + inst + '_' + alg + '*.trace'
+    seed = 0
     for name in glob.glob(path):
-        traces.append(name)
-        print(name)
-    for i in range(100):
-        f = open(traces[i])
+        
+        for k in range(0, 100):
+            if(name[k] == '.'):
+                break
+        k = k + 3
+        for j in range(k,100):
+            if(name[j] == '_'):
+                break
+        traces.append([name, seed])
+        seed = seed + 1 
+
+    nseeds = seed 
+
+    bins = np.zeros((nBins,100, nseeds))
+    count = np.zeros((nBins,100, nseeds))
+
+
+    for i in range(nseeds):
+        f = open(traces[i][0])
+        seed = traces[i][1]
         for line in f:
             t = line.split(', ')
             for i in range(2):
                 t[i] = float(t[i].replace("\n",""))
+            t.append(seed)
             data.append(t)
+            #print(data)
 
     for i in data:
-        relSQ = ((i[1] - exact)/exact)
-        bins[int(np.floor(i[0] * 10)):,int(np.floor(relSQ*100)):] = bins[int(np.floor(i[0] * 10)):,int(np.floor(relSQ*100)):] + 1
-        count[int(np.floor(i[0] * 10)),int(np.floor(relSQ*100))] = count[int(np.floor(i[0] * 10)),int(np.floor(relSQ*100))] + 1
+        relSQ = ((i[1] - exact)/exact)  
+        #Ignore Rel Err above 10%
+        if(i[0]*10 > 110):
+            continue
+        bins[int(np.floor(i[0] * 10)):,int(np.floor(10*relSQ*100)):, i[2]] = 1
 
-    for i in range(110):
-        for j in range(100):
-                bins[i,j] = bins[i,j]/count[:i+1,:j+1].sum()
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    #First Part
+    #(Fixed % relative error asumme < 10)
+    if(alg == 'LS1'):
+        q = 2.4 
+    else:
+        q = 0.8
+    X = np.arange(0, 110, 1)
+    X = X / 10
+    Y = np.zeros((110,))
+
+    
+
+    for i in range(110):      
+        Y[i] = (np.sum(bins[i, int(np.floor(10*q)), :])/nseeds)*100
+
+    plt.figure()
+    plt.plot(X, Y)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Succes (%)")
+    plt.title("Rel Err = " + str(q) + " %")
+    plt.savefig("figs/part1/" + str(alg) + "_" + str(q) + ".png")
+
+    #Second Part
+    #Fixed Cutoff time (in seconds)
+    if(alg == 'LS1'):
+        t = 8 
+    else:
+        t = 5
+    t = int(np.floor(t))
+    X2 = np.arange(0, 10, 0.1)
+    Y2 = np.zeros((np.size(X2)))
+
+    for j in range(0, 100):
+        Y2[j] = (np.sum(bins[t , j, :])/nseeds)*100
+
+    plt.figure()
+    plt.plot(X2, Y2)
+    plt.xlabel("Rel Err (%)")
+    plt.ylabel("Succes (%)")
+    plt.title("Time = " + str(t) + " (s)")
+    plt.savefig("figs/part2/" + str(alg) + "_" + str(t) + ".png")
+
+
+
+
+
+    #for i in range(110):
+    #    for j in range(100):
+    #            bins[i,j] = bins[i,j]/count[:i+1,:j+1].sum()
+
+    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
     # Make data.
-    Y = np.arange(0, 11, 0.1)
-    X = np.arange(0, 100, 1)
-    X, Y = np.meshgrid(X, Y)
+    #Y = np.arange(0, 11, 0.1)
+    #X = np.arange(0, 100, 1)
+    #X, Y = np.meshgrid(X, Y)
 
     # Plot the surface.
-    surf = ax.plot_surface(Y, X, bins, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-    plt.show()
+    #surf = ax.plot_surface(Y, X, bins, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+    #plt.show()
 
