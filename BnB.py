a82fbf7d5fbd62126e65d4cb7849f78a25f7007e
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-12-03 16:00:14 -0500

    BnB in-place
    Change BnB so that graph updates are done in-place.

--- BnB.py
+++ BnB.py
@@ -19,111 +19,98 @@ def BnB(inst, alg, cutOff, rSeed, G):
     random.seed(rSeed)
 
     i = 0  # standard iterator
-    # while (1 and i <= 100):
-    #     if os.path.exists("OutputFiles/" + inst + "_" + alg +
-    #                       "_" + str(cutOff) + "_" + str(i) + ".trace"):
-    #         i = i + 1
-    #         # print("Here")
-    #     else:
-    #         traceFile = open("OutputFiles/" + inst + "_" + alg +
-    #                          "_" + str(cutOff) + "_" + str(i) + ".trace", "x")
-    #         break
     while (1 and i <= 100):
-        if os.path.exists("TestOutput/" + inst + "_" + alg +
+        if os.path.exists("OutputFiles/" + inst + "_" + alg +
                           "_" + str(cutOff) + "_" + str(i) + ".trace"):
             i = i + 1
             # print("Here")
         else:
-            traceFile = open("TestOutput/" + inst + "_" + alg +
+            traceFile = open("OutputFiles/" + inst + "_" + alg +
                              "_" + str(cutOff) + "_" + str(i) + ".trace", "x")
             break
+    # while (1 and i <= 100):
+    #     if os.path.exists("TestOutput/" + inst + "_" + alg +
+    #                       "_" + str(cutOff) + "_" + str(i) + ".trace"):
+    #         i = i + 1
+    #         # print("Here")
+    #     else:
+    #         traceFile = open("TestOutput/" + inst + "_" + alg +
+    #                          "_" + str(cutOff) + "_" + str(i) + ".trace", "x")
+    #         break
 
     # Begin BnB
     # Preprocess G to remove isolated nodes and presort according to node degree
     G.remove_nodes_from(list(nx.isolates(G)))
-    G_sorted = sorted(G.nodes(), key=lambda n: G.degree(n), reverse=True)
-    H = nx.Graph()
-    H.add_nodes_from(G_sorted)
-    H.add_edges_from(G.edges(data=True))
-    G = H
+    # G_sorted = sorted(G.nodes(), key=lambda n: G.degree(n), reverse=True)
+    # H = nx.Graph()
+    # H.add_nodes_from(G_sorted)
+    # H.add_edges_from(G.edges(data=True))
+    # G = H
 
     # Initialize variables
     # Define initial problem
     initial_vertex = list(G.nodes)[0]   # Pick max degree node
-    parent = (-1, -1)                   # Set parent to oob
     B = list(G.nodes)                   # Initialize vc to nodes
     sizeB = len(list(G.nodes))          # Initialize upper bound
     G_prime = G.copy()                  # Create graph for in-place mod
     explored_set = []                   # Explored vertices
     include_set = []                    # Whether vertex is included
 
+    # Initialize Frontier
     frontier = []
-
-    # frontier.append((initial_vertex, True, [], []))
-    frontier.append((initial_vertex, False, [], []))
-    frontier.append((initial_vertex, True, [], []))
+    frontier.append((initial_vertex, False, (-1, False)))
+    frontier.append((initial_vertex, True, (-1, False)))
 
     # frontier_history = []
 
     # Begin Exploring Frontier
     start = time.time()
-    # for i in range(3):
+    # for i in range(25):
     while frontier != [] and (time.time() - start < cutOff):
-        # if len(frontier) == 0:
-        #     break
 
         backtrack = False
 
         # Expand newest problem on frontier
-        # print(frontier[-1])
-        cur_vertex, included, C, C_exc = frontier.pop()
+        # print("Problem: ", frontier[-1])
+        cur_vertex, included, parent = frontier.pop()
 
         # Construct current working graph
-        G_prime = G.copy()
-        if C != []:
-            for node in C:
-                G_prime.remove_node(node)
-        if C_exc != []:
-            for node in C_exc:
-                G_prime.remove_node(node)
+        # Changed, will be done in-place
+        # G_prime = G.copy()
+        # if C!=[]:
+        #     for node in C:
+        #         G_prime.remove_node(node)
+        # if C_exc!=[]:
+        #     for node in C_exc:
+        #         G_prime.remove_node(node)
 
         # Remove vertex from subgraph
         if included == True:
-            C.append(cur_vertex)
-            neighbors = list(G_prime.neighbors(cur_vertex))
-            # C_exc.extend(neighbors)
             G_prime.remove_node(cur_vertex)
-            # for node in neighbors:
-            #     G_prime.remove_node(node)
-
         elif included == False:
-            # C_exc.append(cur_vertex)
             neighbors = list(G_prime.neighbors(cur_vertex))
-            C.extend(neighbors)
-            # G_prime.remove_node(cur_vertex)
             for node in neighbors:
+                include_set.append(True)
+                explored_set.append(node)
                 G_prime.remove_node(node)
-            # print(C, C_exc)
 
-        # Remove vertex from subgraph
-        # if included == True:
-        #     G_prime.remove_node(cur_vertex)
-        # elif included == False:
-        #     neighbors = list(G_prime.neighbors(cur_vertex))
-        #     for node in neighbors:
-        #         include_set.append(True)
-        #         explored_set.append(node)
-        #         G_prime.remove_node(node)
+        # Add current vertex to sets
+        include_set.append(included)
+        explored_set.append(cur_vertex)
 
-        # Regardless, add state
-        # include_set.append(included)
-        # explored_set.append(cur_vertex)
+        # Calculate C according to explored_set
+        # and include_set
+        C = []
+        for i in range(len(explored_set)):
+            if include_set[i] == True:
+                C.append(explored_set[i])
 
         # Check current solution
-        sol = utils.checker(G, C)
-        # if G_prime.number_of_edges() == 0:
-        if sol == True:
+        # sol = utils.checker(G, C) # Safer but slower
+        # if sol == True:
+        if G_prime.number_of_edges() == 0:
             # print("Solution Found")
+            backtrack = True
             if len(C) < len(B):
                 # print("Solution Better")
                 # If soln then update bounds
@@ -131,7 +118,7 @@ def BnB(inst, alg, cutOff, rSeed, G):
                 sizeB = len(B)
                 # Print to trace
                 duration = time.time() - start
-                print(sizeB, duration)
+                # print(sizeB, duration)
                 printTraceFile(len(C), duration, traceFile)
         else:
             # Else
@@ -140,19 +127,62 @@ def BnB(inst, alg, cutOff, rSeed, G):
             # print("Consider subproblem")
             # if len(list(G_prime.nodes)) == 0:
             #     print("Empty Subgraph")
-            if len(list(G_prime.nodes)) > 0:
+            if len(list(G_prime.nodes)) > 0:  # Make sure subgraph isn't empty
                 G_prime_lower_bound = compute_lower_bound_simple(G_prime)
+                # G_prime_lower_bound = compute_lower_bound_simple2(G_prime)
+                # G_prime_lower_bound = compute_lower_bound(
+                #     cutOff, rSeed, G_prime)
                 lower_bound = len(C) + G_prime_lower_bound
                 # print(lower_bound)
                 # print("Consider subproblem, bound: ",
                 #       lower_bound, " sizeB: ", sizeB)
                 if lower_bound < sizeB:
                     # If promising
-                    # print("Add to Frontier")
                     best_ver = max(list(G_prime.degree), key=lambda x: x[1])[0]
-                    # print("Best Vertex: ", best_ver)
-                    frontier.append((best_ver, False, C.copy(), C_exc.copy()))
-                    frontier.append((best_ver, True, C.copy(), C_exc.copy()))
+                    # print("Add vertex ", best_ver, " to Frontier")
+                    frontier.append((best_ver, False, (cur_vertex, included)))
+                    frontier.append((best_ver, True, (cur_vertex, included)))
+                else:
+                    backtrack = True
+
+        if backtrack == True:
+            if frontier != []:
+                # Backtrack to next pending problem in frontier
+                par_vertex, par_included = frontier[-1][2]
+                # print("Reconstructing ver: ", par_vertex,
+                #       "included: ", par_included)
+                if par_vertex in explored_set:
+                    # print("Parent vertex in explored set")
+                    # Determine index in order to remove the
+                    # appropriate number of points from explored
+                    # set and add back to G_prime
+                    index = explored_set.index(par_vertex) + 1
+                    # print("Index: ", index, "Explored Set: ", len(explored_set))
+                    # print("Nodes to add back: ", len(explored_set) - index)
+                    # for i in range(index, len(explored_set)):
+                    while index < len(explored_set):
+                        # Add back vertex
+                        removed_vertex = explored_set.pop()
+                        removed_included = include_set.pop()
+                        G_prime.add_node(removed_vertex)
+                        # print("Added back vertex: ", removed_vertex)
+                        neighbors = G.neighbors(removed_vertex)
+                        subgraph_nodes = G_prime.nodes()
+                        # Shrink Vertex Cover as we go
+                        C = []
+                        for i in range(len(explored_set)):
+                            if include_set[i] == True:
+                                C.append(explored_set[i])
+                        # Add back edges
+                        for neighbor in neighbors:
+                            if neighbor in subgraph_nodes and neighbor not in C:
+                                # print("Added back edge from neighbor: ", neighbor)
+                                G_prime.add_edge(neighbor, removed_vertex)
+
+                elif (par_vertex, par_included) == (-1, False):  # Returned to root node
+                    explored_set = []
+                    include_set = []
+                    G_prime = G.copy()
 
     duration = time.time() - start
 
@@ -167,31 +197,12 @@ def BnB(inst, alg, cutOff, rSeed, G):
 
 ###############################################################
 
-# Find neighbors of neighbors
-
-
-def find_n_of_n(G_prime, neighbors, best_ver):
-    n_of_ = []
-    n_of_n = []
-    for n in neighbors:
-        n_ = list(G_prime.neighbors(n))
-        n_of_.extend(n_)
-
-    n_of_n = list(set(n_of_))
-    n_of_n.remove(best_ver)
-
-    return n_of_n
-
-###############################################################
-
 
 def compute_lower_bound_simple(G):
     # Simple lower bound given by K&T 10.2, pg. 556
     nodes = len(list(G.nodes))
     edges = len(list(G.edges))
     k = int(math.ceil(edges/nodes))
-    # if k > nodes:
-    #     k = nodes
 
     return k
 
@@ -201,12 +212,7 @@ def compute_lower_bound_simple2(G):
     nodes = len(list(G.nodes))
     edges = len(list(G.edges))
     maxedge = max(list(G.degree), key=lambda x: x[1])[1]
-    # if maxedge == 0:
-    #     k = int(math.ceil(edges/nodes))
-    #     return k
     k = int(math.ceil(edges/maxedge))
-    # if k > nodes:
-    #     k = nodes
 
     return k
 ###############################################################
