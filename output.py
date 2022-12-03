f30a51af42689a5ce3f50d9517b91b49b519c704
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-12-03 16:31:52 -0500

    Update output.py
    For BnB and approx do not include rseed in dataset name

--- output.py
+++ output.py
@@ -1,23 +1,37 @@
 import os
 
 # Function to print solution file
+
+
 def printSolutionFile(inst, alg, cutOff, rSeed, qual, MVC):
-    i = 0 # standard iterator
-    while (1 and i <= 100):
-        if os.path.exists("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" + \
-                str(rSeed) + "_" + str(i) + ".sol"):
-            i = i + 1
-        else:
-            f = open("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" + \
-                str(rSeed) + "_" + str(i) + ".sol", "x")
-            break
-    
+    i = 0  # standard iterator
+    if alg == 'BnB' or 'Approx':
+        while (1 and i <= 100):
+            if os.path.exists("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" +
+                              str(i) + ".sol"):
+                i = i + 1
+            else:
+                f = open("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" +
+                         str(i) + ".sol", "x")
+                break
+    else:
+        while (1 and i <= 100):
+            if os.path.exists("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" +
+                              str(rSeed) + "_" + str(i) + ".sol"):
+                i = i + 1
+            else:
+                f = open("OutputFiles/" + inst + "_" + alg + "_" + str(cutOff) + "_" +
+                         str(rSeed) + "_" + str(i) + ".sol", "x")
+                break
+
     f.write(str(qual) + "\n")
-    for i in range(0,qual):
+    for i in range(0, qual):
         f.write(str(MVC[i]) + ",")
-    
+
     f.close()
 
 # Function to update trace file
+
+
 def printTraceFile(qual, time, file):
-    file.write(str('%.4f' % time)+ ", " + str(qual) + "\n")
+    file.write(str('%.4f' % time) + ", " + str(qual) + "\n")
